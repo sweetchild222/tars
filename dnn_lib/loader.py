33576061df62d0556da550d8fe79329c61866bad
sweetchild222 <sweetchild22.ik@gmail.com>
2021-03-08 09:51:00 +0900

    'fix_loader_bug'

--- dnn_lib/loader.py
+++ dnn_lib/loader.py
@@ -4,29 +4,47 @@ import numpy as np
 
 def encodeOneHot(train_t):
 
-  unique = np.unique(train_t, return_counts=False)
-  oneHotMap = {key : index for index, key in enumerate(unique)}
+    unique = np.unique(train_t, return_counts=False)
+    oneHotMap = {key : index for index, key in enumerate(unique)}
 
-  classes = len(oneHotMap)
-  oneHotEncode = [np.eye(classes)[oneHotMap[t]] for t in train_t]
+    classes = len(oneHotMap)
+    oneHotEncode = [np.eye(classes)[oneHotMap[t]] for t in train_t]
 
-  return np.array(oneHotEncode)
+    return np.array(oneHotEncode)
 
 
 def loadTestDataSet(feature_max):
 
-  test_x = []
+    test_x = []
 
-  height = feature_max[0]
-  width = feature_max[1]
+    height = feature_max[0]
+    width = feature_max[1]
 
-  for h in range(height):
-    for w in range(width):
-      feature1 = float(w)
-      feature2 = float(h)
-      test_x.append([feature1, feature2])
+    for h in range(height):
+        for w in range(width):
+            feature1 = float(w)
+            feature2 = float(h)
+            test_x.append([feature1, feature2])
 
     return np.array(test_x)
+
+
+def loadDataSet():
+
+    train_x, train_t, feature_max = extractData()
+
+    train_t = encodeOneHot(train_t)
+
+    test_x = loadTestDataSet(feature_max)
+
+    all_x = np.vstack((train_x, test_x))
+    all_x -= np.mean(all_x)
+    all_x /= np.std(all_x)
+
+    train_x = all_x[0:len(train_x)]
+    test_x = all_x[-len(test_x):]
+
+    return train_x, train_t, test_x, feature_max
 
 
 def extractData():
@@ -53,22 +71,3 @@ def extractData():
             t.append(value)
 
     return np.array(x), np.array(t), img.shape
-
-
-
-def loadDataSet():
-
-    train_x, train_t, feature_max = extractData()
-
-    train_t = encodeOneHot(train_t)
-
-    test_x = loadTestDataSet(feature_max)
-
-    all_x = np.vstack((train_x, test_x))
-    all_x -= np.mean(all_x)
-    all_x /= np.std(all_x)
-
-    train_x = all_x[0:len(train_x)]
-    test_x = all_x[-len(test_x):]
-
-    return train_x, train_t, test_x, feature_max
